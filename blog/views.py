74062e77c377aeb6523b9ff66126786f926424cd
hamzashahid-10p <hamza.shahid@10pearls.com>
2024-01-03 11:42:41 +0500

    add urls and views

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def starting_page(request):
+    pass
+
+def posts(request):
+    pass
+
+def post_detail(request):
+    pass
